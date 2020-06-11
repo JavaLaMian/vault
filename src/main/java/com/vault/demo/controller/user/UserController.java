e653f35f41a5e5092d6007d085f0e98aa8667a54
17870097767 <1829625744@qq.com>
2020-06-11 15:41:57 +0800

    账户中心-账户安全，页面

--- src/main/java/com/vault/demo/controller/user/UserController.java
+++ src/main/java/com/vault/demo/controller/user/UserController.java
@@ -1,6 +1,6 @@
 package com.vault.demo.controller.user;
 
-import com.vault.demo.bean.user.userimf;
+import com.vault.demo.bean.userimf;
 import com.vault.demo.service.user.UserService;
 import org.apache.commons.mail.EmailException;
 import org.springframework.stereotype.Controller;
@@ -26,5 +26,13 @@ public class UserController {
     public String getMa(String email,String type) throws EmailException {
         String ma = service.getEmailMa(email,type);
         return ma;
+    }
+    @RequestMapping("/toAO")
+    public String toAO(){
+        return "user/AccountOverview";
+    }
+    @RequestMapping("/toAS")
+    public String toAS(){
+        return "user/AccountSafe";
     }
 }
