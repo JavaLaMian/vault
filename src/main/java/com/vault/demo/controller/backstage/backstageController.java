7b403e564ad26fadfc92dac298e51364654bab5a
Cemer-ShangHai <283756992@qq.com>
2020-06-15 08:55:14 +0800

    update backloancontroller

--- src/main/java/com/vault/demo/controller/backstage/backstageController.java
+++ src/main/java/com/vault/demo/controller/backstage/backstageController.java
@@ -22,20 +22,35 @@ public class backstageController {
         return mv;
     }
     @RequestMapping("/text")
-    public ModelAndView text(ModelAndView mv){
-        mv.setViewName("backstage/text");
+    public ModelAndView text(ModelAndView mv,HttpSession session){
+        Admin admin = (Admin)session.getAttribute("admin");
+        if(admin == null){
+            mv.setViewName("backstage/admin_login");
+        }else{
+            mv.setViewName("backstage/text");
+        }
         return mv;
     }
     //个人信息页面
     @RequestMapping("/profile")
-    public ModelAndView profile(ModelAndView mv){
-        mv.setViewName("backstage/Bprofile");
+    public ModelAndView profile(ModelAndView mv,HttpSession session){
+        Admin admin = (Admin)session.getAttribute("admin");
+        if(admin == null){
+            mv.setViewName("backstage/admin_login");
+        }else{
+            mv.setViewName("backstage/Bprofile");
+        }
         return mv;
     }
     //修改密码页面
     @RequestMapping("/updatePwd")
-    public ModelAndView updatePwd(ModelAndView mv){
-        mv.setViewName("backstage/BupdatePwd");
+    public ModelAndView updatePwd(ModelAndView mv,HttpSession session){
+        Admin admin = (Admin)session.getAttribute("admin");
+        if(admin == null){
+            mv.setViewName("backstage/admin_login");
+        }else{
+            mv.setViewName("backstage/BupdatePwd");
+        }
         return mv;
     }
     //跳往404页面
@@ -46,20 +61,45 @@ public class backstageController {
     }
     //理财订单页面
     @RequestMapping("/LiCaiDOC")
-    public ModelAndView LiCaiDOC(ModelAndView mv){
-        mv.setViewName("backstage/Blicaidoc");
+    public ModelAndView LiCaiDOC(ModelAndView mv,HttpSession session){
+        Admin admin = (Admin)session.getAttribute("admin");
+        if(admin == null){
+            mv.setViewName("backstage/admin_login");
+        }else{
+            mv.setViewName("backstage/Blicaidoc");
+        }
         return mv;
     }
     //投标类别页面
     @RequestMapping("/BidList")
-    public ModelAndView BidList(ModelAndView mv){
-        mv.setViewName("backstage/BidList");
+    public ModelAndView BidList(ModelAndView mv,HttpSession session){
+        Admin admin = (Admin)session.getAttribute("admin");
+        if(admin == null){
+            mv.setViewName("backstage/admin_login");
+        }else{
+            mv.setViewName("backstage/BidList");
+        }
         return mv;
     }
     //去往投标新增页面
     @RequestMapping("/Badd_Bid")
-    public ModelAndView Badd_Bid(ModelAndView mv){
-        mv.setViewName("backstage/Badd_Bid");
+    public ModelAndView Badd_Bid(ModelAndView mv,HttpSession session){
+        Admin admin = (Admin)session.getAttribute("admin");
+        if(admin == null){
+            mv.setViewName("backstage/admin_login");
+        }else{
+            mv.setViewName("backstage/Badd_Bid");
+        }
+        return mv;
+    }
+    @RequestMapping("/loanList")
+    public ModelAndView toLoanList(ModelAndView mv,HttpSession session){
+        Admin admin = (Admin)session.getAttribute("admin");
+        if(admin == null){
+            mv.setViewName("backstage/admin_login");
+        }else{
+            mv.setViewName("backstage/load_list");
+        }
         return mv;
     }
 }
