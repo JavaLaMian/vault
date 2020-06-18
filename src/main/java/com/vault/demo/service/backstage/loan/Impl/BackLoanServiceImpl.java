b9dcb605980dfa4bed2fcdb226d18e76a4fc54ec
Cemer-ShangHai <283756992@qq.com>
2020-06-18 08:03:23 +0800

    update loan 2020-6-18 8:02

--- src/main/java/com/vault/demo/service/backstage/loan/Impl/BackLoanServiceImpl.java
+++ src/main/java/com/vault/demo/service/backstage/loan/Impl/BackLoanServiceImpl.java
@@ -5,9 +5,16 @@ import com.vault.demo.service.backstage.loan.BackLoanService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class BackLoanServiceImpl implements BackLoanService{
     @Resource
     BackLoanDao backLoanDao;
+
+    @Override
+    public List<Map> selLoan() {
+        return backLoanDao.selLoan();
+    }
 }
