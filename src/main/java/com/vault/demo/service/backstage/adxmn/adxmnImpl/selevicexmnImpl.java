d939ee9c0d89b44bf6cf3cccc7d9845f461dcf8a
Lxm123lxm <2840128673@qq.com>
2020-06-18 16:20:57 +0800

    后台界面6月18下午

--- src/main/java/com/vault/demo/service/backstage/adxmn/adxmnImpl/selevicexmnImpl.java
+++ src/main/java/com/vault/demo/service/backstage/adxmn/adxmnImpl/selevicexmnImpl.java
@@ -59,5 +59,10 @@ public class selevicexmnImpl implements selevicexmn {
         return ok;
     }
 
+    @Override
+    public List<Map> integralList() {
+        return dao.integralList();
+    }
+
 
 }
