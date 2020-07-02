cf7ff6bc5b9a9a8fea99d376ac57e87549569737
VN-Lf <zyqdyyzd@iCloud.com>
2020-07-02 16:39:14 +0800

    3.9 个人中心支付采用支付宝接口

--- src/main/java/com/vault/demo/controller/user/UserController2.java
+++ src/main/java/com/vault/demo/controller/user/UserController2.java
@@ -1,15 +1,28 @@
 package com.vault.demo.controller.user;
 
+import com.alipay.api.AlipayApiException;
+import com.alipay.api.AlipayClient;
+import com.alipay.api.DefaultAlipayClient;
+import com.alipay.api.domain.AlipayTradeWapPayModel;
+import com.alipay.api.internal.util.AlipaySignature;
+import com.alipay.api.request.AlipayTradePagePayRequest;
 import com.vault.demo.bean.*;
+import com.vault.demo.config.AlipayConfig;
 import com.vault.demo.service.user.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -137,22 +150,12 @@ public class UserController2 {
         service.upUser(userimf);
         return "";
     }
-    @RequestMapping("/dlmm")
+    @RequestMapping("/tiXian")
     @ResponseBody
-    public Boolean dlmm(String loginPsw,HttpSession session){
-        Userimf userimf = (Userimf) session.getAttribute("user");
-        userimf.setLoginPsw(loginPsw);
-        System.out.println(userimf);
-        service.upUser(userimf);
-        session.setAttribute("user",userimf);
-        return true;
-    }
-    @RequestMapping("/czTx")
-    @ResponseBody
-    public Map ChongZhi(String type,String money,String pwd,HttpSession session){
-        Userimf userimf = (Userimf) session.getAttribute("user");
-        String uPwd = userimf.getDealPsw();
+    public Map tiXian(String type,String money,String pwd,HttpSession session){
         Map map = new HashMap();
+        Userimf userimf = (Userimf)session.getAttribute("user");
+        String uPwd = userimf.getDealPsw();
         if(uPwd.equals(pwd)){
             int pd = service.userChongTi(type,money,userimf);
             if(pd == 1){
@@ -169,6 +172,117 @@ public class UserController2 {
         }
         return map;
     }
+
+    @RequestMapping("/dlmm")
+    @ResponseBody
+    public Boolean dlmm(String loginPsw,HttpSession session){
+        Userimf userimf = (Userimf) session.getAttribute("user");
+        userimf.setLoginPsw(loginPsw);
+        System.out.println(userimf);
+        service.upUser(userimf);
+        session.setAttribute("user",userimf);
+        return true;
+    }
+
+    @RequestMapping("/czTx")
+    public void ChongZhi(String money,HttpServletResponse httpServletResponse){
+        String commodity = "余额充值";//获取表单提交的商品名称
+        String nowtime = String.valueOf(System.currentTimeMillis());//性能优化  获取当前时间
+        System.out.println("time:"+nowtime);
+
+        // 商户订单号，商户网站订单系统中唯一订单号，必填
+        String out_trade_no = nowtime;//采用时间戳当做订单号
+        // 销售产品码 必填
+        String product_code="FAST_INSTANT_TRADE_PAY";
+        // 付款金额，必填
+        String total_amount=new String(money);
+        // 订单名称，必填
+        String subject = new String(commodity);
+        // 商品描述，可空
+        String body = new String("为你的小母牛理财余额充值");
+        //String passback_params = "merchantBizType%3d3C%26merchantBizNo%3d2016010101111";
+        // 超时时间 可空
+        String timeout_express="2m";
+
+        // SDK 公共请求类，包含公共请求参数，以及封装了签名与验签，开发者无需关注签名与验签
+        //调用RSA签名方式
+        AlipayClient alipayClient =  new DefaultAlipayClient(AlipayConfig.gatewayUrl,AlipayConfig.app_id, AlipayConfig.merchant_private_key, AlipayConfig.FORMAT, AlipayConfig.charset, AlipayConfig.alipay_public_key,AlipayConfig.sign_type);  //获得初始化的AlipayClient
+        AlipayTradePagePayRequest alipayRequest =  new  AlipayTradePagePayRequest(); //创建API对应的request
+        //AlipayTradeWapPayRequest alipayRequest=new AlipayTradeWapPayRequest();
+
+        // 封装请求支付信息
+        AlipayTradeWapPayModel model = new AlipayTradeWapPayModel();
+        model.setOutTradeNo(out_trade_no);//单号
+        model.setSubject(subject);//订单名称，必填
+        model.setTotalAmount(total_amount);//付款金额
+        model.setBody(body);//商品描述
+        model.setTimeoutExpress(timeout_express);//超过时间
+        model.setProductCode(product_code);//销售产品码
+        alipayRequest.setBizModel(model);
+        // 设置异步通知地址
+        alipayRequest.setNotifyUrl(AlipayConfig.notify_url);
+        // 设置同步地址
+        alipayRequest.setReturnUrl(AlipayConfig.return_url);
+
+        // form表单生产
+        String form = "";
+        try {
+            // 调用SDK生成表单
+            form = alipayClient.pageExecute(alipayRequest).getBody();
+            httpServletResponse.setContentType("text/html;charset=" + AlipayConfig.charset);
+            httpServletResponse.getWriter().write(form);//直接将完整的表单html输出到页面
+            httpServletResponse.getWriter().flush();
+            httpServletResponse.getWriter().close();
+        } catch (AlipayApiException | IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    //设置同步地址
+    @GetMapping("/return_url")
+    public String return_url(HttpServletRequest request,HttpSession session) throws UnsupportedEncodingException, AlipayApiException {
+        System.out.println("进来了同步");
+        Map<String,String> params = new HashMap<String,String>();
+        Map<String,String[]> requestParams = request.getParameterMap();
+        for (Iterator<String> iter = requestParams.keySet().iterator(); iter.hasNext();) {
+            String name = (String) iter.next();
+            String[] values = (String[]) requestParams.get(name);
+            String valueStr = "";
+            for (int i = 0; i < values.length; i++) {
+                valueStr = (i == values.length - 1) ? valueStr + values[i]
+                        : valueStr + values[i] + ",";
+            }
+            //乱码解决，这段代码在出现乱码时使用
+            //valueStr = new String(valueStr.getBytes("ISO-8859-1"), "utf-8");
+            params.put(name, valueStr);
+        }
+        //商户订单号
+        String out_trade_no = new String(request.getParameter("out_trade_no").getBytes("ISO-8859-1"),"UTF-8");
+        //支付宝交易号
+        String trade_no = new String(request.getParameter("trade_no").getBytes("ISO-8859-1"),"UTF-8");
+        //付款金额
+        String total_amount = new String(request.getParameter("total_amount").getBytes("ISO-8859-1"),"UTF-8");
+
+        System.out.println("支付宝交易号:"+trade_no+"<br/>商户订单号:"+out_trade_no+"<br/>付款金额:"+total_amount);
+        boolean signVerified = AlipaySignature.rsaCheckV1(params, AlipayConfig.alipay_public_key, AlipayConfig.charset, AlipayConfig.sign_type); //调用SDK验证签名
+        System.out.println("同步SDK"+signVerified);
+        //——请在这里编写您的程序（以下代码仅作参考）——
+        if(signVerified) {//验证成功
+            System.out.println("验证成功！");
+            Userimf userimf = (Userimf)session.getAttribute("user");
+            int pd = service.userChongTi("cz",total_amount,userimf);
+            if(pd == 1){
+                Userimf user = service.pandEmail(userimf.getEmail(),"e");
+                session.setAttribute("user",user);
+            }
+            return "redirect:toAO";//跳转到订单信息页面
+        }else {
+            System.out.println("验证失败");
+            return "index";
+        }
+    }
+
     @RequestMapping("/unbindCard")
     @ResponseBody
     public Boolean unbindCard(UserBank bank,HttpSession session){
