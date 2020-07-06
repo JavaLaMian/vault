799aaa2caf09478db332c38ba83e8ca7639b0dad
Tom <1642558447@qq.com>
2020-07-06 16:29:15 +0800

    贷款

--- src/main/java/com/vault/demo/dao/PerBidDao.java
+++ src/main/java/com/vault/demo/dao/PerBidDao.java
@@ -2,12 +2,16 @@ package com.vault.demo.dao;
 
 import com.vault.demo.bean.Loan;
 import com.vault.demo.bean.PerBid;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
 
 @Repository
 @Transactional
 public interface PerBidDao {
-    PerBid selectPerBidByPerBidId(Loan loan);
+    List<Map> selectPerBidByPerBidId(@Param("loan") Loan loan);
     void updatePerBidStatus(PerBid perBid);
 }
